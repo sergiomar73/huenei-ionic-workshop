ef015c53522b2527c1334706127e885deef03ad6
Sergio <sergiomar73@gmail.com>
2018-08-22 02:09:47 -0300

    Lugares

---
+++ 4-clima-lugares/src/pages/lugares/lugares.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { IonicPage, AlertController } from 'ionic-angular';
+
+// v4
+import { Lugar } from '../../models/lugar'
+import { LugaresProvider } from '../../providers/lugares/lugares';
+import { GeocodeProvider } from '../../providers/geocode/geocode';
+
+@IonicPage()
+@Component({
+  selector: 'page-lugares',
+  templateUrl: 'lugares.html',
+})
+export class LugaresPage {
+
+  public lugares: Lugar[];
+  public vista: string = 'list';
+
+  constructor(
+    private lugaresService: LugaresProvider, // v4
+    private geocodeService: GeocodeProvider, // v4
+    private alertCtrl: AlertController
+  ) {
+    this.lugaresService.getLugares().then((res: Lugar[]) => {
+      this.lugares = res;
+    });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LugaresPage');
+  }
+
+  // v4 Botón Nuevo Lugar
+  public nuevoLugar(): void {
+    let prompt = this.alertCtrl.create({
+      title: 'Nueva Ciudad',
+      message: "Ingresá el nombre de la Ciudad",
+      inputs: [
+        {
+          name: 'ciudad',
+          placeholder: 'Ciudad, Provincia, País'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: data => {
+            console.log('Cancelado');
+          }
+        },
+        {
+          text: 'Agregar',
+          handler: data => {
+            console.log(`Ciudad ingresada -> ${data.ciudad}`);
+            if (data.ciudad != '') {
+              this.geocodeService.getUbicacion(data.ciudad).then((res: Lugar) => {
+                this.lugaresService.agregarLugar(res);
+              });
+            }
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  public borrarLugar(lugar: Lugar) {
+    this.lugaresService.borrarLugar(lugar);
+  }
+
+}
